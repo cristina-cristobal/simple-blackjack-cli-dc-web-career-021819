672cb506fce64c614458671c8b2a703641114015
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-05 19:59:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,9 +1,11 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
 def display_card_total
@@ -11,7 +13,7 @@ def display_card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -41,4 +43,3 @@ end
 def runner
   # code runner here
 end
-    
